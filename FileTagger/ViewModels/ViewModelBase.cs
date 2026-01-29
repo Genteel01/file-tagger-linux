1f954f4e8e6e7da7d732f087977af57b77687c2f
Genteel01 <georgeshepherd3@gmail.com>
2026-01-29 18:56:53 +1100

    Initial commit

---
+++ FileTagger/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FileTagger.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ FileTagger/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FileTagger.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
