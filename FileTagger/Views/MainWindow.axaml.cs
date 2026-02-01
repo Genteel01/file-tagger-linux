03f52212e977755d2bfd48466e9ed61c5de5ec0d
Genteel01 <georgeshepherd3@gmail.com>
2026-02-01 17:18:18 +1100

    Temperature converter tutorial

--- FileTagger/Views/MainWindow.axaml.cs
+++ FileTagger/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 
 namespace FileTagger.Views;
 
@@ -7,5 +9,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+        {
+            Fahrenheit.Text = "";
+        }
+        else if (double.TryParse(Celsius.Text, out double C))
+        {
+            double F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
     }
 }
