31b57715ad0b98cb26aa69a305eb34d05235eb9d
Genteel01 <georgeshepherd3@gmail.com>
2026-07-16 17:19:53 +1000

    To do list tutorial complete

--- FileTagger/App.axaml.cs
+++ FileTagger/App.axaml.cs
@@ -3,20 +3,27 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using FileTagger.ViewModels;
 using FileTagger.Views;
+using FileTagger.Services;
 
 namespace FileTagger;
 
 public partial class App : Application
 {
+    // This is a reference to our MainViewModel which we use to save the list on shutdown. You can also use Dependency Injection
+    // in your App.
+    private readonly MainWindowViewModel _mainViewModel = new MainWindowViewModel();
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
@@ -25,11 +32,53 @@ public partial class App : Application
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = _mainViewModel
             };
+            desktop.ShutdownRequested += DesktopOnShutdownRequested;
         }
 
         base.OnFrameworkInitializationCompleted();
+
+        // Init the MainViewModel
+        await InitMainViewModelAsync();
+    }
+
+    // Optional: Load data from disc
+    private async Task InitMainViewModelAsync()
+    {
+        // get the items to load
+        var itemsLoaded = await FileService.LoadFromFileAsync();
+
+        if (itemsLoaded is not null)
+        {
+            foreach (var item in itemsLoaded)
+            {
+                _mainViewModel.ToDoItems.Add(new ToDoItemViewModel(item));
+            }
+        }
+    }
+
+    // We want to save our ToDoList before we actually shutdown the App. As File I/O is async, we need to wait until file is closed
+    // before we can actually close this window
+
+    private bool _canClose; // This flag is used to check if window is allowed to close
+    private async void DesktopOnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+    {
+        e.Cancel = !_canClose; // cancel closing event first time
+
+        if (!_canClose)
+        {
+            // To save the items, we map them to the ToDoItem-Model which is better suited for I/O operations
+            var itemsToSave = _mainViewModel.ToDoItems.Select(item => item.GetToDoItem());
+            await FileService.SaveToFileAsync(itemsToSave);
+
+            // Set _canClose to true and Close this Window again
+            _canClose = true;
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
     }
 
     private void DisableAvaloniaDataAnnotationValidation()
