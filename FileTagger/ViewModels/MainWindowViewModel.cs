2b623483c7f6b3933d4ab1e8bcbd722e95e66e25
Genteel01 <georgeshepherd3@gmail.com>
2026-07-29 14:00:15 +1000

    Introduced metadata package

--- FileTagger/ViewModels/MainWindowViewModel.cs
+++ FileTagger/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,8 @@ using CommunityToolkit.Mvvm.Input;
 using FileTagger.Models;
 using FileTagger.Services;
 using Microsoft.Extensions.DependencyInjection;
+using ATL;
+using ATL.Logging;
 
 namespace FileTagger.ViewModels;
 
@@ -116,6 +118,7 @@ public partial class MainWindowViewModel : ViewModelBase
     [RelayCommand]
     private async Task OpenMusicFiles(CancellationToken token)
     {
+        ConsoleLogger log = new ConsoleLogger();
         ErrorMessages?.Clear();
         try
         {
@@ -127,9 +130,24 @@ public partial class MainWindowViewModel : ViewModelBase
             FileText = "";
             foreach (IStorageFile file in files)
             {
-                if (file.Name.EndsWith(".mp3") ||  file.Name.EndsWith(".wav") || file.Name.EndsWith(".flac"))
+                string fileName = file.Name.ToLower();
+                if (fileName.EndsWith(".mp3") ||  fileName.EndsWith(".wav") || fileName.EndsWith(".flac"))
                 {
-                    FileText += file.Name + " || " + file.Path + "\n";
+                    Track track = new Track(file.Path.LocalPath);
+
+                    FileText += "Absolute Path: " + file.Path.AbsolutePath + "\n ";
+                    FileText += "Local Path: " + file.Path.LocalPath + "\n ";
+                    FileText += "Title: " + track.Title + "\n ";
+                    FileText += "Album: " + track.Album + "\n ";
+                    FileText += "Artist: " + track.Artist + "\n ";
+                    FileText += "Track Number: " + track.TrackNumber + "\n ";
+                    FileText += "Album Artist: " + track.AlbumArtist + "\n ";
+                    FileText += "Year: " + track.Year + "\n ";
+                    FileText += "Genre: " + track.Genre + "\n ";
+                    foreach (string key in track.AdditionalFields.Keys)
+                    {
+                        FileText += key + ": " + track.AdditionalFields[key] + "\n ";
+                    }
                 }
             }
         }
