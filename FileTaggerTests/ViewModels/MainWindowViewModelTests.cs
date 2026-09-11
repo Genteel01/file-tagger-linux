4e5ed3a4601497248cd585c0df4e26c5fd0e8383
Genteel01 <georgeshepherd3@gmail.com>
2026-09-11 20:53:15 +1000

    Added test for if OpenMusicFiles simply runs with the fake file service

--- FileTaggerTests/ViewModels/MainWindowViewModelTests.cs
+++ FileTaggerTests/ViewModels/MainWindowViewModelTests.cs
@@ -1,6 +1,28 @@
+using FileTagger.Services;
+using FileTagger.ViewModels;
+using FileTaggerTests.Fakers;
+
 namespace FileTaggerTests.ViewModels;
 
 public class MainWindowViewModelTests
 {
-    
+    private static MainWindowViewModel CreateMockViewModel()
+    {
+        IFileService fileService = new FakeFileService();
+        PreferenceService preferenceService = new PreferenceService(fileService);
+        IImageService imageService = new FakeImageService();
+        EditPanelViewModel editPanelViewModel = new EditPanelViewModel(fileService, imageService);
+        MainWindowViewModel mainWindowViewModel = new MainWindowViewModel(fileService, preferenceService, editPanelViewModel);
+        return mainWindowViewModel;
+    }
+
+    [Fact]
+    public void OpenMusicFiles_FakeFileService_Runs()
+    {
+        MainWindowViewModel viewModel = CreateMockViewModel();
+
+        viewModel.OpenMusicFilesCommand.Execute(CancellationToken.None);
+    }
+
+
 }
