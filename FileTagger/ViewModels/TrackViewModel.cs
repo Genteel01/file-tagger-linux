6de77317ffa0dba8f0a36f4745a5d0f404f90729
Genteel01 <georgeshepherd3@gmail.com>
2026-09-13 02:17:44 +1000

    Change EmbeddedPictures from List to ObservableCollection, adding extension methods for used list methods that didn't exist on ObservableCollection

--- FileTagger/ViewModels/TrackViewModel.cs
+++ FileTagger/ViewModels/TrackViewModel.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using ATL;
@@ -77,8 +77,7 @@ public partial class TrackViewModel : ViewModelBase
     [ObservableProperty]
     private string _comment;
 
-    [ObservableProperty]
-    private List<PictureInfo> _embeddedPictures;
+    public ObservableCollection<PictureInfo> EmbeddedPictures { get; }
 
     /// <summary>
     /// Gets or sets whether the track has changed
@@ -114,7 +113,7 @@ public partial class TrackViewModel : ViewModelBase
         AlbumArtist = track.AlbumArtist;
         Composer = track.Composer;
         Comment = track.Comment;
-        EmbeddedPictures = track.EmbeddedPictures.Where(pic => pic.NativeFormat != ImageFormat.Unsupported).ToList();
+        EmbeddedPictures = [.. track.EmbeddedPictures.Where(pic => pic.NativeFormat != ImageFormat.Unsupported)];
         _finishedSetup = true;
     }
 
