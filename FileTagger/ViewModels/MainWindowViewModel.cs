ffc2b1d08300d13c66aa2c62cfcf74a386136106
Genteel01 <georgeshepherd3@gmail.com>
2026-09-17 18:32:21 +1000

    Fixed the RowBorderBrushConverter sometimes not having a theme value due to Application.Current.ActualThemeVariant being null

--- FileTagger/ViewModels/MainWindowViewModel.cs
+++ FileTagger/ViewModels/MainWindowViewModel.cs
@@ -95,6 +95,11 @@ public partial class MainWindowViewModel : ViewModelBase
     /// </summary>
     [ObservableProperty] private ThemeVariant _selectedTheme;
 
+    /// <summary>
+    /// Actual <see cref="ThemeVariant"/> of the application
+    /// </summary>
+    [ObservableProperty] private ThemeVariant _actualTheme;
+
     [RelayCommand]
     private void ChangeSelectedTheme(ThemeVariant value)
     {
@@ -142,6 +147,19 @@ public partial class MainWindowViewModel : ViewModelBase
         EditPanelWidth = double.IsPositiveInfinity(preferences.EditPanelWidth) ? GridLength.Star : new GridLength(preferences.EditPanelWidth);
         Themes = [ThemeVariant.Default, ThemeVariant.Light, ThemeVariant.Dark];
         SelectedTheme = Application.Current?.RequestedThemeVariant ?? ThemeVariant.Default;
+        ActualTheme = Application.Current?.ActualThemeVariant ?? ThemeVariant.Default;
+        Application.Current?.ActualThemeVariantChanged += (sender, _) =>
+        {
+            if (sender is not Application app) return;
+            //Despite ActualThemeVariant being a non-nullable type, it CAN be null.
+            //Switching to ThemeVariant.Default triggers ActualThemeVariantChanged twice,
+            //the first time with ActualThemeVariant = null, and the second time with either Light or Dark
+            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+            if (app.ActualThemeVariant != null)
+            {
+                ActualTheme = app.ActualThemeVariant;
+            }
+        };
     }
 
     protected override void OnPropertyChanged(PropertyChangedEventArgs e)
@@ -170,6 +188,7 @@ public partial class MainWindowViewModel : ViewModelBase
         ListColumnWidths = new AvaloniaDictionary<string, double>();
         Themes = [ThemeVariant.Default, ThemeVariant.Light, ThemeVariant.Dark];
         SelectedTheme = ThemeVariant.Default;
+        ActualTheme = ThemeVariant.Default;
     }
     #endif
 
