e031717c26a34339189da3ee269d76dd379a6b24
Genteel01 <georgeshepherd3@gmail.com>
2026-09-20 01:22:53 +1000

    Set up the PicTypeSelector ComboBox with input passthrough on its Popup, maintaining correct Close behaviour

--- FileTagger/Views/EditPanel.axaml.cs
+++ FileTagger/Views/EditPanel.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.LogicalTree;
 using FileTagger.ViewModels;
@@ -31,6 +32,44 @@ public partial class EditPanel : UserControl
         AlbumArtistField.TextFilter = _searchFunction;
         ComposerField.TextFilter = _searchFunction;
         DiscNumberField.TextFilter = _searchFunction;
+    }
+
+    /// <summary>
+    /// <see cref="PicTypeSelector"/>'s Popup
+    /// </summary>
+    private Popup? _popup;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        PicTypeSelector.TemplateApplied += PicTypeSelectorTemplateApplied;
+    }
+
+    /// <summary>
+    /// Set up the popup to allow pointer passthrough
+    /// </summary>
+    private void PicTypeSelectorTemplateApplied(object? sender, TemplateAppliedEventArgs e)
+    {
+        _popup = e.NameScope.Get<Popup>("PART_Popup");
+        _popup.OverlayInputPassThroughElement = TopLevel.GetTopLevel(this);
+        _popup.LostFocus += PicTypeSelectorDropDownLostFocus;
+    }
+
+    /// <summary>
+    /// Set up the popup to refocus itself after clicking one of PicTypeSelector's buttons and to close the popup
+    /// when it loses focus. Don't close the popup when the new focus is the ComboBox or a ComboBoxItem,
+    /// because it closes itself in those cases
+    /// </summary>
+    private void PicTypeSelectorDropDownLostFocus(object? sender, FocusChangedEventArgs e)
+    {
+        if (e.NewFocusedElement is Button b && b.Parent == PicTypeSelector.Parent)
+        {
+            _popup?.Focus();
+        }
+        else if(e.NewFocusedElement != PicTypeSelector && e.NewFocusedElement is not ComboBoxItem)
+        {
+            _popup?.Close();
+        }
     }
 
     private void AutoCompleteBoxDropdownClosed(object? sender, EventArgs e)
