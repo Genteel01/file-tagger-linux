d79fcbfc5a74d3308b13b4424ef0e5e208a51f47
Genteel01 <georgeshepherd3@gmail.com>
2026-09-21 02:35:11 +1000

    Replaced occurrences of SelectedTrackImages[DisplayedImageIndex] with a new property

--- FileTagger/ViewModels/EditPanelViewModel.cs
+++ FileTagger/ViewModels/EditPanelViewModel.cs
@@ -360,7 +360,7 @@ public partial class EditPanelViewModel: ViewModelBase, IRecipient<MainWindowVie
     /// </summary>
     private int RemoveCurrentlyDisplayedImage(TrackViewModel track)
     {
-        PictureInfo displayedImage = SelectedTrackImages[DisplayedImageIndex];
+        PictureInfo displayedImage = SelectedImage;
         int index = track.EmbeddedPictures.FindIndex(pic => pic.TrueEqual(displayedImage));
         if (index != -1)
         {
@@ -385,8 +385,7 @@ public partial class EditPanelViewModel: ViewModelBase, IRecipient<MainWindowVie
             if (!HasSelectedTracks || !IsShowingTrackImages) return false;
             if(CopiedPic == null) return false;
             if(TracksToCutFrom.Count < SelectedTracks.Count) return false;
-            PictureInfo displayedImage = SelectedTrackImages[DisplayedImageIndex];
-            if (!CopiedPic.TrueEqual(displayedImage)) return false;
+            if (!CopiedPic.TrueEqual(SelectedImage)) return false;
             return SelectedTracks.All(track => TracksToCutFrom.Any(cutTrack => cutTrack == track));
         }
     }
@@ -428,7 +427,7 @@ public partial class EditPanelViewModel: ViewModelBase, IRecipient<MainWindowVie
     private void CopyCoverImage()
     {
         CopiedPic = null;
-        CopiedPic = SelectedTrackImages[DisplayedImageIndex];
+        CopiedPic = SelectedImage;
         TracksToCutFrom = [];
     }
 
@@ -592,11 +591,15 @@ public partial class EditPanelViewModel: ViewModelBase, IRecipient<MainWindowVie
     public string PicCountString => $"{DisplayedImageIndex + 1}/{SelectedTrackImages.Count}";
 
     /// <summary>
-    /// Bitmap of the entry in <see cref="SelectedTrackImages"/> to display based on <see cref="DisplayedImageIndex"/>,
-    /// showing <see cref="_imageService"/>.GetDefaultImage() if SelectedTrackImages is empty.
+    /// The PictureInfo of the image that is currently being displayed
+    /// </summary>
+    private PictureInfo SelectedImage => SelectedTrackImages[DisplayedImageIndex];
+
+    /// <summary>
+    /// Bitmap of <see cref="SelectedImage"/>, showing <see cref="_imageService"/>.GetDefaultImage() if SelectedTrackImages is empty.
     /// Updates when SelectedTrackImages or DisplayedImageIndex change
     /// </summary>
-    public Bitmap CurrentDisplayedImage => IsShowingTrackImages ? GetAndCacheBitmap(SelectedTrackImages[DisplayedImageIndex]) : _imageService.GetDefaultImage();
+    public Bitmap CurrentDisplayedImage => IsShowingTrackImages ? GetAndCacheBitmap(SelectedImage) : _imageService.GetDefaultImage();
 
     /// <summary>
     /// Whether to show the navigation buttons for moving between images. Updates when <see cref="SelectedTrackImages"/> changes
