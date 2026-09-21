9419b0a33cd82293027912b04a3ba6c4f04193f5
Genteel01 <georgeshepherd3@gmail.com>
2026-09-21 15:54:15 +1000

    Fixed AreEqualConverter so it can handle value types

--- FileTagger/Converters/AreEqualConverter.cs
+++ FileTagger/Converters/AreEqualConverter.cs
@@ -6,7 +6,8 @@ using Avalonia.Data.Converters;
 namespace FileTagger.Converters;
 
 /// <summary>
-/// Returns True if all values are equal, returns false otherwise. Returns True if there are 0 or 1 values.
+/// Returns True if all values are equal, returns false otherwise.
+/// Returns True if there are 0 or 1 values.
 /// </summary>
 public class AreEqualConverter : IMultiValueConverter
 {
@@ -17,7 +18,8 @@ public class AreEqualConverter : IMultiValueConverter
             object? firstValue = values[0];
             for (int i = 1; i < values.Count; i++)
             {
-                if (values[i] != firstValue) return false;
+                object? currentValue = values[i];
+                if (!Equals(currentValue, firstValue)) return false;
             }
         }
         return true;
