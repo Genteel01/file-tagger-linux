b60f5f4250bf6c11c3663a400f4c2c2b3a329b34
Genteel01 <georgeshepherd3@gmail.com>
2026-09-22 17:16:35 +1000

    Removed tests that can no longer run with new access modifiers. What they were testing is implicitly tested by other tests already

--- FileTaggerTests/ViewModels/EditPanelViewModelTests.cs
+++ FileTaggerTests/ViewModels/EditPanelViewModelTests.cs
@@ -11,33 +11,8 @@ public class EditPanelViewModelTests
     {
         IFileService fileService = new FakeFileService();
         IImageService imageService = new FakeImageService();
-        EditPanelViewModel editPanelViewModel = new EditPanelViewModel(fileService, imageService);
+        EditPanelViewModel editPanelViewModel = new EditPanelViewModel(fileService, imageService, () => null);
         return editPanelViewModel;
-    }
-
-    [Fact]
-    public void ReceiveMessage_NoTracks_NoError()
-    {
-        EditPanelViewModel viewModel = CreateMockViewModel();
-        MainWindowViewModel.SelectedItemsMessage message = new MainWindowViewModel.SelectedItemsMessage([]);
-
-        viewModel.Receive(message);
-
-        Assert.Empty(viewModel.SelectedTracks);
-    }
-
-    [Fact]
-    public void ReceiveMessage_WithTracks_Assigned()
-    {
-        EditPanelViewModel viewModel = CreateMockViewModel();
-        List<TrackViewModel> stubTracks = [StubCreators.CreateStubTrackViewModel()];
-        MainWindowViewModel.SelectedItemsMessage message = new MainWindowViewModel.SelectedItemsMessage(stubTracks);
-
-        viewModel.Receive(message);
-
-        Assert.Equal(stubTracks.Count, viewModel.SelectedTracks.Count);
-        Assert.Equal(stubTracks[0], viewModel.SelectedTracks[0]);
-        Assert.Equal(stubTracks, viewModel.SelectedTracks);
     }
 
     [Fact]
