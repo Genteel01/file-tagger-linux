3d80358770bfe609cbd2abb5dc1e80e9a15b2ebb
Genteel01 <georgeshepherd3@gmail.com>
2026-09-23 20:06:24 +1000

    Updated PicDescriptionDialog to use KeyBindings instead of KeyDown

--- FileTagger/Dialogs/PicDescriptionDialog.axaml.cs
+++ FileTagger/Dialogs/PicDescriptionDialog.axaml.cs
@@ -1,5 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace FileTagger.Dialogs;
@@ -16,17 +15,5 @@ public partial class PicDescriptionDialog : Window
         base.OnLoaded(e);
         InputBox.Focus();
         InputBox.CaretIndex = InputBox.Text?.Length ?? 0;
-    }
-
-    private void DialogKeyDown(object? sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter)
-        {
-            SaveButton.Command?.Execute(null);
-        }
-        else if (e.Key == Key.Escape)
-        {
-            CancelButton.Command?.Execute(null);
-        }
     }
 }
