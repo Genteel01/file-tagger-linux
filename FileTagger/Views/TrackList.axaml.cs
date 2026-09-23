94261a6225671060aea33e23b80d3426faf9f245
Genteel01 <georgeshepherd3@gmail.com>
2026-09-23 18:19:50 +1000

    Added Select All hotkey to list and reworked clearing the selection to account for you now being able to select items without focusing any of them

--- FileTagger/Views/TrackList.axaml.cs
+++ FileTagger/Views/TrackList.axaml.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using FileTagger.Controls;
@@ -46,8 +48,7 @@ public partial class TrackList : UserControl
     }
 
     /// <summary>
-    /// We want the ScrollBars to be Focusable so clicking them won't meet the condition in <see cref="ScrollViewerFocusGained"/>,
-    /// but we don't want them to actually take focus from whatever you were focusing before, so we redirect it back
+    /// Keep the previous element focused when you scroll
     /// </summary>
     private void ScrollBarGettingFocus(object? sender, FocusChangingEventArgs e)
     {
@@ -63,11 +64,11 @@ public partial class TrackList : UserControl
     }
 
     /// <summary>
-    /// Reset the selection when we Focus the ScrollViewer
+    /// Reset the selection when we tap the empty part of the ScrollViewer
     /// </summary>
-    private void ScrollViewerFocusGained(object? sender, FocusChangedEventArgs e)
+    private void ScrollViewerTapped(object? sender, TappedEventArgs e)
     {
-        if (e.NewFocusedElement == sender)
+        if (e.Source is ScrollContentPresenter)
         {
             TrackListBox.UnselectAll();
         }
