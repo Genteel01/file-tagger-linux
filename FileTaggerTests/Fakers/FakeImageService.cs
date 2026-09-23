a88bb2e4c6e4da5831050973817d0571c11bd1c6
Genteel01 <georgeshepherd3@gmail.com>
2026-09-23 18:56:03 +1000

    Updated FakeImageService with new interface changes

--- FileTaggerTests/Fakers/FakeImageService.cs
+++ FileTaggerTests/Fakers/FakeImageService.cs
@@ -6,11 +6,6 @@ namespace FileTaggerTests.Fakers;
 
 public class FakeImageService : IImageService
 {
-    public Bitmap GetDefaultImage()
-    {
-        return new Bitmap("");
-    }
-
     public PictureInfo CreatePictureInfoFromBitmap(Bitmap bitmap, PictureInfo.PIC_TYPE pictureType)
     {
         return new PictureInfo(PictureInfo.PIC_TYPE.Front);
@@ -19,5 +14,11 @@ public class FakeImageService : IImageService
     public PictureInfo CreatePictureInfoFromStream(Stream stream, PictureInfo.PIC_TYPE pictureType)
     {
         return new PictureInfo(PictureInfo.PIC_TYPE.Front);
+    }
+
+    public Bitmap GetBitmap(PictureInfo picInfo)
+    {
+        using MemoryStream ms = new MemoryStream(picInfo.PictureData);
+        return new Bitmap(ms);
     }
 }
